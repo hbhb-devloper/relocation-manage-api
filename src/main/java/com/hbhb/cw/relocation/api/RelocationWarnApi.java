9fbdfc29e73c10ef1cf6160b52d6aaa90cade9f2
wangxiaogang <15151269@163.com>
2020-11-13 09:54:56 +0800

    预警统计

--- src/main/java/com/hbhb/cw/relocation/api/RelocationWarnApi.java
+++ src/main/java/com/hbhb/cw/relocation/api/RelocationWarnApi.java
@@ -1,9 +1,14 @@
 package com.hbhb.cw.relocation.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RelocationWarnApi {
 
     @PostMapping("/add")
     void addWarn();
+
+    @GetMapping("/count")
+    int getWarnCount(@RequestParam("userId") Integer userId);
 }
